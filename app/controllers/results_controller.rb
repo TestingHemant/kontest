65a6da9c7058dc570201952910367aec5166b667
hemant <hemant.kumar@infibeam.net>
2016-12-27 19:31:49 +0530

    Winners: moving entries query under if condition to avoid running on page load

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -13,15 +13,17 @@ class ResultsController < ApplicationController
 		    @contests = Contest.search(params[:search]).order("created_at DESC")
 		    if @contests == ""
 		    	flash[:notice] = "Contests not found, please check id"
-		    end
+		    else
+		    	@entries = Entry.find_by_sql("SELECT e.*, if(e.entryvotes is NULL, count(v.id), e.entryvotes) as vote_count from entries e inner join votes v on e.id = v.entry_id where e.contest_id=#{params[:search]} group by e.id order by vote_count DESC limit 20")
+		    	if !@entries
+					flash[:notice] = "No Entries found"
+				end
+			end
 		end
 		#@entries = Entry.where('contest_id = ?',params[:search]).limit(20)
 		
-		@entries = Entry.find_by_sql("SELECT e.*, if(e.entryvotes is NULL, count(v.id), e.entryvotes) as vote_count from entries e inner join votes v on e.id = v.entry_id where e.contest_id=#{params[:search]} group by e.id order by vote_count DESC")
+		#@entries = Entry.find_by_sql("SELECT e.*, if(e.entryvotes is NULL, count(v.id), e.entryvotes) as vote_count from entries e inner join votes v on e.id = v.entry_id where e.contest_id=#{params[:search]} order by vote_count DESC").limit(20)
 		
-		if !@entries
-			flash[:notice] = "No Entries found"
-		end
 		#@entries.sort { |e1,e2| e1.votes.length <=> e2.votes.length }
 		
 	end
