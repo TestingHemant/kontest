f340ef65f6d9d248f8270946207a800d202895f0
hemant <hemant.kumar@infibeam.net>
2016-12-27 18:04:49 +0530

    Winners: considering entryvotes while fetching winners

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -15,10 +15,14 @@ class ResultsController < ApplicationController
 		    	flash[:notice] = "Contests not found, please check id"
 		    end
 		end
-		@entries = Entry.where('contest_id = ?',params[:search]).limit(20)
+		#@entries = Entry.where('contest_id = ?',params[:search]).limit(20)
+		
+		@entries = Entry.where('contest_id = ?',params[:search]).find_by_sql("SELECT e.*, if(e.entryvotes is NULL, count(v.id), e.entryvotes) as vote_count from entries e inner join votes v on e.id = v.entry_id where e.contest_id='6' group by e.id order by vote_count DESC")
+		
 		if !@entries
-			flash[:notice] = "Test"
+			flash[:notice] = "No Entries found"
 		end
-		@entries.sort { |e1,e2| e1.votes.length <=> e2.votes.length }
+		#@entries.sort { |e1,e2| e1.votes.length <=> e2.votes.length }
+		
 	end
 end
