93991056f238be220980b788701f34c4f85d4d50
hemant <hemant.kumar@infibeam.net>
2017-03-15 19:15:53 +0530

    Login not required to check entries now

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,6 +1,6 @@
 class EntriesController < ApplicationController
   before_action :set_entry, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: :show
   # GET /entries
   # GET /entries.json
   def index
