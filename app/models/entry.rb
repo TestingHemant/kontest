99caa56c89d2840d371d05fc66d459d655008f1d
hemant <hemant.kumar@infibeam.net>
2017-03-22 13:03:02 +0530

    Entry: Adding JPG/JPEG image format in accepted format list

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -13,6 +13,6 @@ class Entry < ActiveRecord::Base
 
   	has_attached_file :image, styles: { medium: "300x300", thumb: "100x100" }
   	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
-  	validates_attachment_file_name :image, matches: [/png\Z/, /jpe?g\Z/]
+  	validates_attachment_file_name :image, matches: [/png\Z/, /jpe?g\Z/, /JPE?G\Z/]
   	validates :image, :presence => true
 end
