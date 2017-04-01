ab4d192311019fcffa765933b98523cdb4114a10
hemant <hemant.kumar@infibeam.net>
2017-04-01 07:11:51 +0530

    Adding data type for entryvotes column

--- db/migrate/20170329172656_add_default_value_entryvotes.rb
+++ db/migrate/20170329172656_add_default_value_entryvotes.rb
@@ -1,5 +1,5 @@
 class AddDefaultValueEntryvotes < ActiveRecord::Migration
   def change
-	change_column :entries, :entryvotes, :default => 0
+	change_column :entries, :entryvotes, :integer, :default => 0
   end
 end
